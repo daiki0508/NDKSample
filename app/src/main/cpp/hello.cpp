4ba50d59bace452eb5e00c8a7d760e70d1d6865b
daiki0508 <oochani05085@outlook.jp>
2021-04-27 11:18:19 +0900

    Update codes

--- app/src/main/cpp/hello.cpp
+++ app/src/main/cpp/hello.cpp
@@ -1,11 +1,16 @@
 #include <jni.h>
-
+#include <string.h>
+#include <string>
 //
 // Created by oocha on 2021/04/27.
 //
 
 extern "C" jstring JNICALL
-Java_com_websarva_wings_android_ndksample_MainActivity_getMessage(JNIEnv *env, jobject thiz) {
+Java_com_websarva_wings_android_ndksample_MainActivity_getMessage(JNIEnv *env, jobject thiz,jstring j_name) {
     // TODO: implement getMessage()
-    return env->NewStringUTF("hello world");
+    const char* name = env->GetStringUTFChars(j_name, 0);
+    std::string msg = "Hello ";
+    msg+= name;
+    msg += "\nWelcome to JNI World!!";
+    return env->NewStringUTF(msg.c_str());
 }
--- app/src/main/java/com/websarva/wings/android/ndksample/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/ndksample/MainActivity.java
@@ -3,21 +3,60 @@ package com.websarva.wings.android.ndksample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+    private EditText editText;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity",getMessage());
+        editText = findViewById(R.id.edit);
+        editText.addTextChangedListener(this);
     }
 
-    public native String getMessage();
+    public void Execute_NDK(View view){
+        String name = editText.getText().toString();
+        String result = getMessage(name);
+        textView = findViewById(R.id.result_text);
+        textView.setText(result);
+    }
+
+    public native String getMessage(String msg);
 
     static {
         System.loadLibrary("hello");
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        String name = editable.toString();
+        Button button = findViewById(R.id.button);
+
+        // ユーザーのニックネームが未入力の場合はボタンを有効にしない
+        if (name.length() > 0){
+            button.setEnabled(true);
+        }else {
+            button.setEnabled(false);
+        }
     }
 }
