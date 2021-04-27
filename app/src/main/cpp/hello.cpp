afad82f3b5cb981bcf741652de3d6861c28ec420
daiki0508 <oochani05085@outlook.jp>
2021-04-27 23:25:27 +0900

    Generate apk file

--- app/src/main/cpp/hello.cpp
+++ app/src/main/cpp/hello.cpp
@@ -1,14 +1,14 @@
 #include <jni.h>
-#include <string.h>
 #include <string>
+#include <string.h>
 //
 // Created by oocha on 2021/04/27.
 //
 
-extern "C" jstring JNICALL
+extern "C" JNIEXPORT jstring JNICALL
 Java_com_websarva_wings_android_ndksample_MainActivity_getMessage(JNIEnv *env, jobject thiz,jstring j_name) {
     // TODO: implement getMessage()
-    const char* name = env->GetStringUTFChars(j_name, 0);
+    const char *name = env->GetStringUTFChars(j_name, 0);
     std::string msg = "Hello ";
     msg+= name;
     msg += "\nWelcome to JNI World!!";
